3ee9531194acb3c9d4df8bfb635def30fa69c067
liam Broadbent <lbroadbent21+github@gmail.com>
2020-06-06 21:27:46 +0100

    added title

--- pages/index.vue
+++ pages/index.vue
@@ -69,6 +69,20 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data () {
+    return {
+      title: 'Liam Broadbent - Website coming soon!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'A Holding page, I use various parts of the site as a sandbox to test various technologies out' }
+      ]
+    }
   }
 }
 </script>
