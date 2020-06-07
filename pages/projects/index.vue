8742f309a15b58085f7081fa21fdb37ad1a0c839
liam Broadbent <lbroadbent21+github@gmail.com>
2020-06-07 19:30:53 +0100

    dev: testing universal mode

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -15,7 +15,6 @@ export default {
     return {
       title: this.title,
       meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
         { hid: 'description', name: 'description', content: 'A Holding page, I use various parts of the site as a sandbox to test various technologies out' }
       ]
     }
