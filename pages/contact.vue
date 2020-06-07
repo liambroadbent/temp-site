82d3d4d8d570189202e08ddd99203747f85faad3
liam Broadbent <lbroadbent21+github@gmail.com>
2020-06-07 16:42:31 +0100

    changes titles on new pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,7 +5,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  components: {
+    Logo
+  },
+  data () {
+    return {
+      title: 'Contact Liam Broadbent!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'A Holding page, I use various parts of the site as a sandbox to test various technologies out' }
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
