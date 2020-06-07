b35cb9f30506c0fea93d0f9fc839e2b9660f882b
liam Broadbent <lbroadbent21+github@gmail.com>
2020-06-07 19:55:29 +0100

    testing GA via production vars

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,21 +1,37 @@
 <template>
-    <div class="container">
-        <h1>Projects Page</h1>
+  <div class="container">
+    <div class="title">
+      <h1>Projects</h1>
     </div>
+    <div class="body">
+      <h3>My Projects</h3>
+      <ul>
+        <li>BMR Calculator</li>
+        <li>Other</li>
+        <li>Other</li>
+      </ul>
+      <p>I plan to fill the page out more, make each project a card with a link offsite?</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       title: 'Various Projects - Liam Broadbent'
     }
   },
-  head () {
+  head() {
     return {
       title: this.title,
       meta: [
-        { hid: 'description', name: 'description', content: 'A Holding page, I use various parts of the site as a sandbox to test various technologies out' }
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'A Holding page, I use various parts of the site as a sandbox to test various technologies out'
+        }
       ]
     }
   }
@@ -23,5 +39,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
