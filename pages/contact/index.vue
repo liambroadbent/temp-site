b5562b7dff7c5b60bcf00278fb21fa341da40f03
liam Broadbent <lbroadbent21+github@gmail.com>
2021-04-27 20:34:31 +0100

    added a google form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,28 +1,58 @@
 <template>
   <div class="container">
-    <h1>Contact Page</h1>
+    <div class="form">
+      <h1>Contact Page</h1>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSff9TbIUcv3JYzu0D7ZGo_dm_-x8Lbx9hTaYszWSacAKuWywQ/viewform?embedded=true"
+        width="640"
+        height="440"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        >Loading…</iframe
+      >
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      title: 'Contact Liam Broadbent!'
+      title: 'Contact Liam Broadbent!',
     }
   },
-  head () {
+  head() {
     return {
       title: this.title,
       meta: [
-        { hid: 'description', name: 'description', content: 'A Holding page, I use various parts of the site as a sandbox to test various technologies out' }
-      ]
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'A Holding page, I use various parts of the site as a sandbox to test various technologies out',
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 @import 'assets/css/scss';
+.container {
+  h1{
+    margin-bottom: 5rem;
+  }
 
+  .form {
+    padding:10rem;
+    min-height:50vw;
+  flex-direction: column;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
